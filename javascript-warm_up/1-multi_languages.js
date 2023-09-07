98befa56fbf60e8c0a2abd37f42512c50726e466
Maester-jeff <jeffndungu45@gmail.com>
2023-09-07 05:57:21 +0300

    1-multi

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,10 +1,5 @@
-const lines = [
-    'C is fun',
-    'Python is cool',
-    'Javascript is amazing'
-];
-let i = 0;
-while (i < lines.length) {
-    console.log(lines[i]);
-    i++;
-}
+console.log('C is fun');
+
+console.log('Python is cool');
+
+console.log('Javascript is amazing');
