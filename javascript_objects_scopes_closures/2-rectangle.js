e92e203a3e0b3035f28d9951a51ff55ec2ebad65
Maester-jeff <jeffndungu45@gmail.com>
2023-09-07 19:51:43 +0300

    2-rectangle.js

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -2,10 +2,10 @@
 class Rectangle {
     constructor (w, h) {
         if (w <= 0 && h <= 0) {
+            Object.assign (this, {}); 
+        }   else {
             this.width = w;
             this.height = h;
-        }   else {
-            Object.assign (this, {});
         }
     }
 }
