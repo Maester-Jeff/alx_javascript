e1c184150229a9cef9ada72d295a3a23650e0100
Maester-jeff <jeffndungu45@gmail.com>
2023-09-12 23:37:52 +0300

    0

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -6,8 +6,11 @@ const id = process.argv[3];
 const url = '${process.argv[2]}${id}'
 
 req.get(url, { encoding: 'utf-8' })
-    .on('data', function (data) {
-        const response = JSON.parse(data);
-        console.log('Code:', response.statusCode);
+    .on('data', data => {
+        console.log(data)
+    })
+
+    .on('response', response => {
+        console.log('Code:', response.statusCode)
     })
 
