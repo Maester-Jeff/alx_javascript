8142e990c798a55b02cd530b3507a3176281863d
Maester-jeff <jeffndungu45@gmail.com>
2023-09-12 22:17:15 +0300

    0-statuscode

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
 
 const req = require('request');
+const id = process.argv[3];
+const url = '${process.argv[2]}${id}'
 
-req.get(url, {encoding: 'utf-8'})
-    .on('response', response => {
-        console.log('Code:', statusCode);
+req.get(url, { encoding: 'utf-8' })
+    .on('data', function (data) {
+        console.log('Code:', data);
     })
