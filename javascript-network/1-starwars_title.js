4682e90091d33b4bd27c78a783722eb27848d35e
Maester-jeff <jeffndungu45@gmail.com>
2023-09-13 00:23:15 +0300

    1

--- javascript-network/1-starwars_title.js
+++ javascript-network/1-starwars_title.js
@@ -4,9 +4,10 @@ const req = require('request');
 
 const id = process.argv[2];
 
-const url = 'https://swapi-api.alx-tools.com/api/films/:id';
+const url = 'https://swapi-api.alx-tools.com/api/films/${id}';
 
-req.get(url, (body) => {
+req.get(url, (response, body) => {
         const object = JSON.parse(body);
+        console.log('code:', response.statusCode);
         console.log(object.title);
     });
