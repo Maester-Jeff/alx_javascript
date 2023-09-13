4806caec6501fa6d3003231eb869fb60c76d0aaa
Maester-jeff <jeffndungu45@gmail.com>
2023-09-13 21:12:26 +0300

    2

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -14,6 +14,6 @@ req.get(url, (error, response, body) => {
     const movies = JSON.parse(body).results;
     const wedgeAntilles = movies.filter((movie => 
         movie.characters.includes(`https://swapi-api.alx-tools.com/api/films/${characterId}`)
-    );
+    ));
     console.log(wedgeAntilles.length);
 });
