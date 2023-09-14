6a5aed25e43fc032ca3e67b1536c6558e2477792
Maester-jeff <jeffndungu45@gmail.com>
2023-09-14 20:17:02 +0300

    3

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -9,11 +9,6 @@ const filePath = process.argv[3];
 
 request.get(url, (error, response, body) => {
     fs.writeFile(filePath, body, 'utf-8', (writeError) => {
-        if (writeError) {
-            console.error(writeError);
-            process.exit[1];
-        }
         console.log()
     });
-
 });
