4b79c11ed1d5bbddd20934838129f27027d20d6a
Maester-jeff <jeffndungu45@gmail.com>
2023-09-14 23:15:03 +0300

    0-welcome

---
+++ javascript_advanced/0-welcome.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+function welcome(firstName, lastName) {
+    var fullName = firstName + ' ' + lastName;
+  
+    function displayFullName() {
+      alert('Welcome ' + fullName + '!');
+    }
+
+    displayFullName();
+  }
+  
