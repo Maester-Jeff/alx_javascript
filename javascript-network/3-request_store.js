4b023e992a74c762f319b6df53dadd2d053e2fe1
Maester-jeff <jeffndungu45@gmail.com>
2023-09-14 20:26:18 +0300

    3

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-
+/*
 const request = require('request');
 const fs = require('fs');
 
@@ -11,4 +11,37 @@ request.get(url, (error, response, body) => {
     fs.writeFile(filePath, body, 'utf-8', (writeError) => {
         console.log()
     });
+});
+*/
+
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+if (!url || !filePath) {
+  console.error('Please provide both a URL and a file path as arguments.');
+  process.exit(1);
+}
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error('An error occurred:', error);
+    process.exit(1);
+  }
+
+  if (response.statusCode !== 200) {
+    console.error('Request failed with status code:', response.statusCode);
+    process.exit(1);
+  }
+
+  fs.writeFile(filePath, body, 'utf-8', (writeError) => {
+    if (writeError) {
+      console.error('Error writing to the file:', writeError);
+      process.exit(1);
+    }
+
+    console.log(`Contents of ${url} saved to ${filePath}`);
+  });
 });
