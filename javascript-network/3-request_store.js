a4450555bba41cc890aa127934a757d7d803ffdc
Maester-jeff <jeffndungu45@gmail.com>
2023-09-14 20:00:58 +0300

    3

---
+++ javascript-network/3-request_store.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+
+const filePath = process.argv[3];
+
+request.get(url, (error, response, body) => {
+    fs.writefile(filePath, body, utf-8, (writeError) => {
+        if (writeError) {
+            console.error(writeError);
+            process.exit[1];
+        }
+        console.log(`${url} saved to ${filePath}`)
+    });
+
+});
