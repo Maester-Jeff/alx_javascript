ce841e0339e15db6c832de1eb77844bbb63683de
Maester-jeff <jeffndungu45@gmail.com>
2023-09-14 22:09:30 +0300

    4

--- javascript-network/4-completed_tasks.js
+++ javascript-network/4-completed_tasks.js
@@ -1,28 +1,4 @@
 #!/usr/bin/node
-/*
-const request = require('request')
-
-const url = process.argv[2];
-
-request(url, function (error, response, body) {
-    if (error) {
-        console.log(error)
-    } else {
-    const todos = JSON.parse(body);
-    const completedTasks = {};
-    for (const todo of todos) {
-        if (todo.complete) {
-            if (completedTasks[todo.userID]) {
-              completedTasks[todo.userID] += 1;
-            } else {
-                completedTasks[todo.userId] = 1;
-            }
-        }
-    }
-    console.log(completedTasks);
-    }
-});
-*/
 
 const request = require('request');
 
@@ -31,10 +7,10 @@ const apiUrl = process.argv[2];
 request(apiUrl, (error, response, body) => {
     const todosData = JSON.parse(body);
 
-    // Initialize an object to store the count of completed tasks per user.
+    // Initialize an object to store the count of completed tasks.
     const completedTasks = {};
 
-    // Iterate through the todos and count completed tasks for each user.
+    // Iterate through the todos and count completed tasks.
     todosData.forEach((todo) => {
       if (todo.completed) {
         if (completedTasks[todo.userId]) {
